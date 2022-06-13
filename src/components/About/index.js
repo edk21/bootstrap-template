df6f1ee5ca87c2ca4ac0facf6996f0827b297dc1
edk21 <edwinmcken@gmail.com>
2022-06-13 19:50:22 +0200

    services section added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import "./About.css"
 
 const About = () => {
   return (
-    <section id="about">
-        <Container fluid className="aboutContainer">
-            About
+    <section id="about" className='about'>
+        <Container className="aboutContainer">
+            <div className='section-title'>
+              <h2>About</h2>
+              <h3>Learn More <span>About Us</span></h3>
+              <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+            </div>
+            <Row className="content">
+              <Col lg={6}>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <ul>
+                  <li>
+                    <i className="fa-solid fa-check-double"></i>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check-double"></i>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-check-double"></i>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat
+                  </li>
+                </ul>
+              </Col>
+              <Col lg={6} className="pt-4 pt-lg-0">
+                <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" className='btn-learn-more'>Learn More</a>
+              </Col>
+            </Row>
         </Container>
     </section>
   )
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,12 +1,54 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import "./Services.css"
+
+const servicesItems = [
+    {
+        icon: 'fa-solid fa-basketball',
+        title: 'Team Player',
+        text: 'Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi'
+    },
+    {
+        icon: 'fa-solid fa-file-lines',
+        title: 'Project Management',
+        text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore'
+    },
+    {
+        icon: 'fa-solid fa-gauge-high',
+        title: 'Time Management',
+        text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia'
+    },
+    {
+        icon: 'fa-solid fa-earth-africa',
+        title: 'Woldwide Reach',
+        text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis'
+    },
+]
 
 const Services = () => {
     return (
         <section id="services">
-            <Container fluid className="servicesContainer">
-                services
+            <Container className="servicesContainer">
+                <div className="section-title">
+                    <h2>services</h2>
+                    <h3>We Do Offer Awesome <span>Services</span></h3>
+                    <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+                </div>
+                <Row>
+                    {
+                        servicesItems.map((item, index) => (
+                            <Col key={index} md={6} lg={3} className="d-flex align-items-stretch mb-5 mb-lg-0">
+                                <div className="icon-box">
+                                    <div className="icon">
+                                        <i className={item.icon}></i>
+                                    </div>
+                                    <h4 className="title">{item.title}</h4>
+                                    <p className="description">{item.text}</p>
+                                </div>
+                            </Col>
+                        ))
+                    }
+                </Row>
             </Container>
         </section>
     )
