2c2d0a7f708fc314f26a925c031e64e4f7cc45a7
edk21 <edwinmcken@gmail.com>
2022-06-14 12:10:41 +0200

    faq section added

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Accordion, Stack } from 'react-bootstrap'
+import ComponentHeader from '../ComponentHeader'
+import { accordionItems } from './accordionItems'
 import "./Faq.css"
+
+const title= "F.a.q"
+const header= "Frequently Asked"
+const headerSpan= "Questions"
+const text = ""
 
 const Faq = () => {
   return (
-    <section id="faq">
-        <Container fluid className="faqContainer">
-            FAQ
+    <section id="faq" className="faq">
+        <Container className="faqContainer">
+            <ComponentHeader title={title} header={header} headerSpan={headerSpan} text={text} />
+            <div className="faq-list">
+              {
+                accordionItems.map((item, index) => (
+                  <Accordion key={index}>
+                    <Accordion.Item>
+                      <Accordion.Header>{item.question}</Accordion.Header>
+                      <Accordion.Body>{item.answer}</Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                ))
+              }
+            </div>
         </Container>
     </section>
   )
